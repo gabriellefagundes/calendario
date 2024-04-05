9ca3dd0b459b802482bc6c242a2486be7752ee6f
Larissa Gabrielle Fagundes Andrade <larissa.gabrielle@alunoescolassociais.org.br>
2024-04-05 11:49:27 -0300

    edita js

--- calendar.js
+++ calendar.js
@@ -2,7 +2,7 @@ function colorirDia(){
     let days       = document.getElementById("day").value;
     let color      = document.getElementById("color").value;
     let calendar   = document.getElementById("calendar");
-    let td         = calendar.getElementsByTagName("td")[(parseInt(days)+1)];
+    let td         = calendar.getElementsByTagName("td")[(parseInt(days))];
 
     td.style.backgroundColor =color;
 }
